7ee1aa478a84ed0d16cbca3c045aee144f6165d0
gered <gered@blarg.ca>
2014-03-29 12:54:03 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# clj-figlet
+
+A Clojure library to generate text banners in an 'ASCII Art' style using FIGlet fonts, similar to other
+[FIGlet programs](http://www.figlet.org/).
+
+## Usage
+
+FIXME
+
+## License
+
+Distributed under the the MIT License. See LICENSE for more details.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-figlet "0.1.0-SNAPSHOT"
+  :description "Library to generate text banners in an 'ASCII Art' style using FIGlet fonts."
+  :url "https://github.com/gered/clj-figlet"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
