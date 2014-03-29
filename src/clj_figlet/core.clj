4455bb6d65b7a9ccc426461d9d20d40f242a689f
gered <gered@blarg.ca>
2014-03-29 13:35:16 -0400

    begin implementing a flf loader

--- src/clj_figlet/core.clj
+++ src/clj_figlet/core.clj
@@ -1,6 +1,16 @@
-(ns clj-figlet.core)
+(ns clj-figlet.core
+  (:import (java.io File))
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- parse-flf-header [header-line]
+  (let [parts (str/split header-line #" ")]
+    {:hardblank    (-> parts first last)
+     :height       (Integer/parseInt (nth parts 1))
+     :num-comments (Integer/parseInt (nth parts 5))}))
+
+(defn load-flf [file]
+  (let [lines      (str/split (slurp file) #"\n")
+        header     (parse-flf-header (first lines))
+        char-lines (drop (inc (:num-comments header)) lines)]
+    ))
+
